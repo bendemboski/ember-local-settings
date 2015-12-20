e661d88386f5bc8d2c2e4c0214738018b7894bea
Ben Demboski <ben@joltlabs.com>
2015-12-20 00:59:10 -0800

    Fix initializer

--- app/initializers/local-settings.js
+++ app/initializers/local-settings.js
@@ -1,7 +1,16 @@
 import ENV from '../config/environment';
 
-export function initialize(application) {
-  const { localSettings: config = {} } = ENV;
+export function initialize() {
+  // Depending on the context the first argument might be the registry, or it
+  // might be the application.
+  const application = arguments[1] || arguments[0];
+  const {
+    localSettings: config = {
+      serializer: 'json',
+      adapter: 'local-storage',
+      prefix: 'emberApp/'
+    }
+  } = ENV;
 
   application.register('config:local-settings', config, { instantiate: false });
   application.inject('service:local-settings', 'config', 'config:local-settings');
